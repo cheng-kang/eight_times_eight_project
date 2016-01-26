229a2c0d1b70d4875d77a11c7d33b3e731ae660b
Ant <me@chengkang.pw>
2016-01-26 07:03:37 +0000

    添加适用的bootcamp代码

---
+++ eight_times_eight_project/auth_new/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from eight_times_eight_project.auth_new.forms import SignUpForm
+from django.contrib.auth.models import User
+from eight_times_eight_project.feeds.models import Feed
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if not form.is_valid():
+            return render(request, 'auth_new/signup.html', {'form': form})
+        else:
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            User.objects.create_user(username=username, password=password, email=email)
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            welcome_post = u'{0} has joined the network.'.format(user.username, user.username)
+            feed = Feed(user=user, post=welcome_post)
+            feed.save()
+            return redirect('/')
+    else:
+        return render(request, 'auth_new/signup.html', {'form': SignUpForm()})
--- eight_times_eight_project/urls.py
+++ eight_times_eight_project/urls.py
@@ -1,10 +1,25 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'eight_times_eight_project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+    url(r'^$', 'eight_times_eight_project.core.views.home', name='home'),
+    url(r'^login', 'django.contrib.auth.views.login', {'template_name': 'core/cover.html'}, name='login'),
+    url(r'^logout', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
+    url(r'^signup/$', 'eight_times_eight_project.auth_new.views.signup', name='signup'),
+    url(r'^settings/$', 'eight_times_eight_project.core.views.settings', name='settings'),
+    url(r'^settings/picture/$', 'eight_times_eight_project.core.views.picture', name='picture'),
+    url(r'^settings/upload_picture/$', 'eight_times_eight_project.core.views.upload_picture', name='upload_picture'),
+    url(r'^settings/save_uploaded_picture/$', 'eight_times_eight_project.core.views.save_uploaded_picture', name='save_uploaded_picture'),
+    url(r'^settings/password/$', 'eight_times_eight_project.core.views.password', name='password'),
+    url(r'^network/$', 'eight_times_eight_project.core.views.network', name='network'),
+    url(r'^feeds/', include('eight_times_eight_project.feeds.urls')),
+    url(r'^messages/', include('eight_times_eight_project.messages_new.urls')),
+    url(r'^notifications/$', 'eight_times_eight_project.activities.views.notifications', name='notifications'),
+    url(r'^notifications/last/$', 'eight_times_eight_project.activities.views.last_notifications', name='last_notifications'),
+    url(r'^notifications/check/$', 'eight_times_eight_project.activities.views.check_notifications', name='check_notifications'),
+    url(r'^search/$', 'eight_times_eight_project.search.views.search', name='search'),
+    url(r'^(?P<username>[^/]+)/$', 'eight_times_eight_project.core.views.profile', name='profile'),
+    url(r'^i18n/', include('django.conf.urls.i18n', namespace='i18n')),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
